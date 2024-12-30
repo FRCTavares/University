a027635b11e8b1e67fc9b63ad6f9b5af9b7671d9
Francisco <francisco.carreira.tavares@gmail.com>
2024-12-30 00:55:30 +0000

    DL - HW2

--- 1 - Deep Learning/Homework 2/hw2_skeleton_code/hw2-q2/hw2-q2.py
+++ 1 - Deep Learning/Homework 2/hw2_skeleton_code/hw2-q2/hw2-q2.py
@@ -29,11 +29,12 @@ class ConvBlock(nn.Module):
 
         # Q2.1. Initialize convolution, maxpool, activation and dropout layers 
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        # Q2.2 Initialize batchnorm layer 
+        self.bn = nn.BatchNorm2d(out_channels) if batch_norm else nn.Identity()
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.dropout_layer = nn.Dropout(0.1)
         
-        # Q2.2 Initialize batchnorm layer 
         #raise NotImplementedError
 
     def forward(self, x):
@@ -60,17 +61,27 @@ class CNN(nn.Module):
         self.batch_norm = batch_norm
 
         # Initialize convolutional blocks
-        self.block1 = ConvBlock(channels[0], channels[1], 3)
-        self.block2 = ConvBlock(channels[1], channels[2], 3)
-        self.block3 = ConvBlock(channels[2], channels[3], 3)
+        self.block1 = ConvBlock(channels[0], channels[1], 3, batch_norm=batch_norm)
+        self.block2 = ConvBlock(channels[1], channels[2], 3, batch_norm=batch_norm)
+        self.block3 = ConvBlock(channels[2], channels[3], 3, batch_norm=batch_norm)
+
+        # Global average pooling instead of flattening
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        # BarchNorm in MLP block (Identify if not batch_norm)
+        self.bn_mlp = nn.BatchNorm1d(channels[3]) if batch_norm else nn.Identity()
         
         # Initialize layers for the MLP block
         # For Q2.2 initalize batch normalization
-        self.fc1 = nn.Linear(128 * 6 * 6, fc1_out_dim)
+        self.fc1 = nn.Linear(channels[3], fc1_out_dim)
+        self.bn_fc1 = nn.BatchNorm1d(fc1_out_dim) if batch_norm else nn.Identity()
         self.relu1 = nn.ReLU()
         self.drop1 = nn.Dropout(0.1)
+
         self.fc2 = nn.Linear(fc1_out_dim, fc2_out_dim)
+        self.bn_fc2 = nn.BatchNorm1d(fc2_out_dim) if batch_norm else nn.Identity()
         self.relu2 = nn.ReLU()
+        
         self.fc3 = nn.Linear(fc2_out_dim, 6)  # Example: 6 classes
 
     def forward(self, x):
@@ -80,20 +91,29 @@ class CNN(nn.Module):
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
+
+        # For Q2.2 implement global averag pooling
+        # Global average pooling
+        x = self.global_avg_pool(x) # (B, 128, 1, 1)
+        x = x.squeeze(-1).squeeze(-1) # (B, 128)
         
         # Flattent output of the last conv block
-        x = x.view(x.size(0), -1)
+        #x = x.view(x.size(0), -1)
 
         # Implement MLP part
+        # BatchNorm in MLP 
+        x = self.bn_mlp(x)
+
         x = self.fc1(x)
+        x = self.bn_fc1(x)
         x = self.relu1(x)
         x = self.drop1(x)
+
         x = self.fc2(x)
+        x = self.bn_fc2(x)
         x = self.relu2(x)
+
         x = self.fc3(x)
-        
-        # For Q2.2 implement global averag pooling
-        
 
         return F.log_softmax(x, dim=1)
  
